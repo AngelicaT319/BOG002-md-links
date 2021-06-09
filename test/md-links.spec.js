2d0355d0427434880d28385ce5cff1920a3a9bee
AngelicaTheran <atheran082@gmail.com>
2021-06-09 15:10:46 -0500

    realizando test unitarios

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,27 +1,45 @@
-const mdLinks = require('../md-links');
+const { default: axios } = require("axios");
+const mdLinks = require("../index.js");
 
-
-describe('mdLinks', () => {
-  expect(typeof mdLinks).toBe(Object)
-
-  // it('debe leer un archivo', () => {    
-  //   return expect(mdLinks.leerArchivos("./prueba.md")).resolves.toBe([
-  //     {
-  //       path: './prueba.md',
-  //       link: 'https://github.com/hola-workshopper/learnyounode',
-  //       text: 'learnyounode'
-  //     },
-  //     {
-  //       path: './prueba.md',
-  //       link: 'https://docs.npmjs.com/getting-started/what-is-npm',
-  //       text: 'NPM'
-  //     },
-  //     {
-  //       path: './prueba.md',
-  //       link: 'https://docs.npmjs.com/getting-started/what-is-npm',
-  //       text: 'NPM'
-  //     }
-  //   ])
-  //  });
-
+describe("mdlinks", () => {
+  it("mdlinks es una funcion", () => {
+    expect(typeof mdLinks).toBe("object");
+  });
 });
+
+test("Resuelve en un array con la data del archivo", () => {
+  return mdLinks.api.leerArchivos("./prueba.md").then((data) => {
+    expect(data).toStrictEqual([
+      {
+        path: "./prueba.md",
+        link: "https://github.com/hola-workshopper/learnyounode",
+        text: "learnyounode",
+      },
+      {
+        path: "./prueba.md",
+        link: "https://docs.npmjs.com/getting-started/what-is-npm",
+        text: "NPM",
+      },
+      {
+        path: "./prueba.md",
+        link: "https://docs.npmjs.com/getting-started/what-is-npm",
+        text: "NPM",
+      },
+    ]);
+  });
+});
+
+// describe("mdLinks", () =>{
+//   test("deberia mostrar el numero de archivos", ()=>{
+//     expect(mdLinks.api.statusLinks([1])).toStrictEqual(1)
+//   })  
+// })
+ jest.mock("axios");
+ it("Deberia retornar status links", ()=>{
+const resp = [{element: status}];
+
+axios.get.mockResolvedValue(resp);
+
+return status.All().then(data => expect(data).toEqual(status));
+ }) 
+
